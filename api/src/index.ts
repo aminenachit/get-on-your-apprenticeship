0646a8499bbd28a112b0901036e9aded7b7cccb2
amine <aminenachit98@gmail.com>
2024-05-24 17:23:10 +0200

    update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,68 +1,41 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
 import app from './app';
 import debug from 'debug';
-debug('api:server');
-
 import http from 'http';
 
-/**
- * Get port from environment and store in Express.
- */
+debug('api:server');
 
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
-
 const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
-
 function normalizePort(val: string) {
   const port = parseInt(val, 10);
 
   if (isNaN(port)) {
-    // named pipe
     return val;
   }
 
   if (port >= 0) {
-    // port number
     return port;
   }
 
   return false;
 }
 
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error: Error & { syscall: string; code: string }) {
+function onError(error: NodeJS.ErrnoException) {
   if (error.syscall !== 'listen') {
     throw error;
   }
 
   const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-  // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges');
@@ -76,10 +49,6 @@ function onError(error: Error & { syscall: string; code: string }) {
       throw error;
   }
 }
-
-/**
- * Event listener for HTTP server "listening" event.
- */
 
 function onListening() {
   const addr = server.address();
