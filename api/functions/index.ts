b8fe5e8e62e91ddfdecebdaf8f22e9ef1bb6db9b
amine <aminenachit98@gmail.com>
2024-05-24 11:48:06 +0200

    Configure API for deployment on Netlify

---
+++ api/functions/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import serverless from 'serverless-http';
+import realRouter from '../src/routes/real';
+import userRouter from '../src/routes/users';
+import errorHandler from '../src/error-handler'; // Assurez-vous d'importer vos gestionnaires d'erreurs
+
+const app = express();
+
+app.use(express.json());
+app.use('/real', realRouter);
+app.use('/user', userRouter);
+
+// Utilisez votre middleware de gestion des erreurs personnalisé
+app.use(errorHandler);
+
+// Détecter l'environnement d'exécution
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+
+// Exporter l'application pour Netlify
+module.exports.handler = serverless(app);
