43b53960748930ccdbf91f2cd92fe9293614d772
amine <aminenachit98@gmail.com>
2024-05-24 20:25:19 +0200

    Add CORS support and fix linter warning for api

--- api/functions/index.ts
+++ api/functions/index.ts
@@ -1,19 +1,18 @@
 import express from 'express';
 import serverless from 'serverless-http';
+import cors from 'cors';
 import realRouter from '../src/routes/real';
 import userRouter from '../src/routes/users';
-import errorHandler from '../src/error-handler'; // Assurez-vous d'importer vos gestionnaires d'erreurs
-
+import errorHandler from '../src/error-handler'; 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/real', realRouter);
 app.use('/users', userRouter);
 
-// Utilisez votre middleware de gestion des erreurs personnalisé
 app.use(errorHandler);
 
-// Détecter l'environnement d'exécution
 if (process.env.NODE_ENV !== 'production') {
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
@@ -21,5 +20,4 @@ if (process.env.NODE_ENV !== 'production') {
   });
 }
 
-// Exporter l'application pour Netlify
 module.exports.handler = serverless(app);
